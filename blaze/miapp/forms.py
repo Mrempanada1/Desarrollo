4bad3431367e133db40a41aec52b176f2722da28
Mrempanada1 <101122089+Mrempanada1@users.noreply.github.com>
2024-10-06 23:34:55 -0300

    Delete blaze directory

--- blaze/miapp/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import Vehiculo
-
-
-class VehiculoForm(forms.ModelForm):
-    class Meta:
-        model = Vehiculo
-        fields = ['marca', 'modelo', 'año', 'color',
-                  'patente', 'cliente']  # Campos del formulario
