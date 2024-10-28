2804ac39783117e320479114a71e14378bc92b4d
Mrempanada1 <101122089+Mrempanada1@users.noreply.github.com>
2024-10-27 23:51:26 -0300

    Add files via upload

---
+++ Capstone/blaze/miapp/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from .models import Dueño, Vehiculo, Servicio, Trabajador, Proceso, Pago, Cita, Cotizacion
+
+admin.site.register(Dueño)
+admin.site.register(Vehiculo)
+admin.site.register(Servicio)
+admin.site.register(Trabajador)
+admin.site.register(Proceso)
+admin.site.register(Pago)
+admin.site.register(Cita)
+
+
+class CotizacionAdmin(admin.ModelAdmin):
+    # Ajusta los campos segun lo que quieras mostrar
+    list_display = ('vehiculo', 'estado', 'total_estimado', 'fecha_creacion')
+    # Permite buscar por patente de vehiculo y nombre del servicio
+    search_fields = ('vehiculo__patente',)
+    # Permite filtrar por estado
+    list_filter = ('estado',)
+
+
+admin.site.register(Cotizacion, CotizacionAdmin)
---
+++ Capstone/blaze/miapp/apps.py
@@ -0,0 +1,50 @@
+from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+
+
+class MiappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'miapp'
+    verbose_name = "Gestión de Talleres Automotrices"
+
+    def ready(self):
+        post_migrate.connect(create_groups, sender=self)
+
+
+def create_groups(sender, **kwargs):
+    from django.contrib.auth.models import Group, Permission
+    from django.contrib.contenttypes.models import ContentType
+    from miapp.models import Vehiculo, Proceso, Cotizacion, Cita, Pago, Trabajador, Dueño
+
+    # Definir los grupos y permisos
+    groups = {
+        'Clientes': {
+            'models': [Vehiculo, Proceso, Cotizacion, Cita, Pago],
+            'actions': ['view', 'add'],  # Consultar y registrar
+        },
+        'Administradores': {
+            'models': [Trabajador, Dueño, Proceso, Pago, Cotizacion, Cita],
+            # Consultar, registrar, modificar y eliminar
+            'actions': ['view', 'add', 'change', 'delete'],
+        },
+        'Trabajadores': {
+            'models': [Proceso],
+            'actions': ['view', 'add'],  # Consultar y registrar procesos
+        },
+        'Supervisores': {
+            'models': [Proceso],
+            # Pueden ver y cambiar el estado de los procesos
+            'actions': ['view', 'change'],  # Pueden supervisar los procesos
+        }
+    }
+
+    for group_name, permissions_data in groups.items():
+        group, created = Group.objects.get_or_create(name=group_name)
+        for model in permissions_data['models']:
+            content_type = ContentType.objects.get_for_model(model)
+            for action in permissions_data['actions']:
+                permission = Permission.objects.get(
+                    codename=f'{action}_{model._meta.model_name}',
+                    content_type=content_type
+                )
+                group.permissions.add(permission)
